961b02c39e9c5023265bf1844b1ddd7ca2b3d5b3
Joshua Jancula <joshuajancula@Joshuas-MacBook-Pro.local>
2017-11-06 23:49:26 -0500

    I'm not done with this but it's almost midnight

---
+++ psuedoForRPG.md
@@ -0,0 +1,35 @@
+RPG game
+
+create an interface of images of your players
+
+
+have multiple players 
+    - set the players to have stats such as health, attack strength
+	-have players battle one another
+
+create a way to select a character to be your player and to be the opponent
+	- first click is your player
+	- second is the opponent
+		- if you defeat your opponent move to next opponent, health does not reset but attack increases by 6
+		- have opponents in an array and if statement that says if opponent health is <= 0 move to next opponent
+
+create a way to have your character fight the other
+    	what happens when you click
+		  -when you click make players fight (blow for blow) and subtract health based on opponents strength
+    	
+Add an on click listener to the gems
+create a function to add the onclick of a button to add to the current fight
+  -should be something like onclick makes your guy attack the opponent
+
+
+ create a reset function for the game
+  - all character stats set to default
+  - select new characters
+
+ create a scoreboard for the game
+ 	- how much health remains
+
+ 
+
+ 
+
---
+++ script.js
@@ -0,0 +1,120 @@
+
+
+// wait for document to load
+ $(document).ready(function() {
+
+// create characters as variables
+var char1 = {
+	power: 6,
+	health: 100
+};
+
+var char2 = {
+	power: 8,
+	health: 90
+};
+
+var char3 = {
+	power: 9,
+	health: 85
+};
+
+var char4 = {
+	power: 10,
+	health: 80
+};
+
+ var yourHealth;
+ var opponentHealth;
+ var yourCharacter = 0;
+ var yourPower = 0;
+ var opponentCharacter = 0;
+ var opponentPower = 0;
+
+ 
+
+$(".enemies").hide();
+$(".attack").hide();
+
+// Pick your character
+
+   $(".character").on("click", function() {
+   		yourCharacter = this.value;
+        $(".character").hide();
+        $(this).show();
+        $(".enemies").show();
+        if (yourCharacter == 1) {
+        	$("#char1a").hide();
+        	yourHealth = char1.health;
+        	yourPower = char1.power;
+        } 
+        else if (yourCharacter == 2) {
+        	$("#char2a").hide();
+        	yourHealth = char2.health;
+        	yourPower = char2.power;
+        }
+       else if (yourCharacter == 3) {
+        	$("#char3a").hide();
+        	yourHealth = char3.health;
+        	yourPower = char3.power;
+        }
+       else if (yourCharacter == 4) {
+        	$("#char4a").hide();
+        	yourHealth = char4.health;
+        	yourPower = char4.power;
+        }
+
+  
+});
+
+// Pick your first opponent
+
+ $(".enemies").on("click", function() {
+        $(".enemies").hide();
+        $(this).show();
+        opponentCharacter = this.value;
+        $(".attack").show();
+        if (opponentCharacter == 1) {
+        	opponentHealth = char1.health;
+        	opponentPower = char1.power;
+        } 
+        else if (opponentCharacter == 2) {
+        	opponentHealth = char2.health;
+        	opponentPower = char2.power;
+        }
+       else if (opponentCharacter == 3) {
+        	opponentHealth = char3.health;
+        	opponentPower = char3.power;
+        }
+       else if (opponentCharacter == 4) {
+        	opponentHealth = char4.health;
+        	opponentPower = char4.power;
+        }
+        
+   
+});
+
+
+// Attack button
+
+  $(".attack").on("click", function() {
+       opponentHealth = opponentHealth - yourPower;
+       yourHealth = yourHealth - opponentPower;
+       $("#opponentHealth").text(opponentHealth);
+       $("#yourHealth").text(yourHealth);
+       if (yourHealth <= 0) {
+       	alert("GAME OVER YOU LOSE");
+       } 
+       if (opponentHealth <= 0) {
+       	alert("YOU WIN THIS ROUND");
+       }
+        
+   
+});
+
+
+
+
+
+});
+   
